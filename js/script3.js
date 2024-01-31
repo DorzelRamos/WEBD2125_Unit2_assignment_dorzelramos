7828dd071fbf66dd3a83aa081f531751144065ad
DorzelRamos <dorzel.ramos@gmail.com>
2024-01-31 16:42:20 -0600

    completed assignment

--- js/script3.js
+++ js/script3.js
@@ -1,14 +1,22 @@
-$.getJSON('data.json',function(data){
-    // console.log(data);
-
-    var output = '<ul class="searchresults">';
-    $.each(data, function(key, val) {
-        output += '<li>';
-        output += '<h2>' + val.name + '</h2>';
-        output += '<img src="planet_images/' + val.name + '_nasa.jpg" alt="' + val.name + '"/>'
-        output += '<p>' + val.bio
-        output += '</li>';
-    });
-    output += '</ul>';
-    $('#container').html(output);
-}); // get JSON
+$('#search').keyup(function(data) {
+    var searchField = $('#search').val();
+    // console.log(searchField);
+    var myExp = new RegExp(searchField, "i");
+    $.getJSON('data.json',function(data){
+        // console.log(data);
+    
+        var output = '<ul class="searchresults">';
+        $.each(data, function(key, val) {
+            if ((val.name.search(myExp) != -1) || 
+                (val.bio.search(myExp) != -1)) {
+                output += '<li>';
+                output += '<h2>' + val.name + '</h2>';
+                output += '<img src="planet_images/' + val.name + '_nasa.jpg" alt="' + val.name + '"/>'
+                output += '<p>' + val.bio
+                output += '</li>';
+            }
+        });
+        output += '</ul>';
+        $('#container').html(output);
+    }); // get JSON
+});
